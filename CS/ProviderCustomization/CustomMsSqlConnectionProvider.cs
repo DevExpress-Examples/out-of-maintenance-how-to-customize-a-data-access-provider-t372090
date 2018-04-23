ea7a5ec282892a3287852bbf9e329d3122ce41b0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ProviderCustomization/CustomMsSqlConnectionProvider.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Threading;
+using DevExpress.DataAccess.Native.Sql.ConnectionProviders;
+using DevExpress.Xpo.DB;
+using DevExpress.Xpo.DB.Helpers;
+
+namespace ConsoleProviderCustomization {
+    public class CustomMsSqlConnectionProvider : DataAccessMSSqlConnectionProvider {
+        public CustomMsSqlConnectionProvider(IDbConnection connection, AutoCreateOption autoCreateOption) : base(connection, autoCreateOption) {}
+
+        const string aliasLead = "[";
+        const string aliasEnd = "]";
+
+        static CustomMsSqlConnectionProvider() {
+            Register();
+            RegisterDataStoreProvider(XpoProviderTypeString, CustomCreateProviderFromString);
+            RegisterDataStoreProvider("System.Data.SqlClient.SqlConnection", CreateCustomProviderFromConnection);
+            RegisterFactory(new CustomMsSqlProviderFactory());
+        }
+
+        public static void RegProvider() {}
+
+        public static IDataStore CustomCreateProviderFromString(string connectionString, AutoCreateOption autoCreateOption, out IDisposable[] objectsToDisposeOnDisconnect) {
+            IDbConnection connection = new SqlConnection(connectionString);
+            objectsToDisposeOnDisconnect = new IDisposable[] {connection};
+            return CreateCustomProviderFromConnection(connection, autoCreateOption);
+        }
+
+        public static IDataStore CreateCustomProviderFromConnection(IDbConnection connection, AutoCreateOption autoCreateOption) {
+            return new CustomMsSqlConnectionProvider(connection, autoCreateOption);
+        }
+
+        public override string FormatColumn(string columnName) {
+            return EncloseAlias(columnName, aliasLead, aliasEnd);
+        }
+
+        public override string FormatColumn(string columnName, string tableAlias) {
+            return string.Format("{0}.{1}", EncloseAlias(tableAlias, aliasLead, aliasEnd), FormatColumn(columnName));
+        }
+
+        public override string FormatTable(string schema, string tableName, string tableAlias) {
+            string encloseAlias = EncloseAlias(tableAlias, aliasLead, aliasEnd);
+            return string.IsNullOrEmpty(encloseAlias)
+                ? FormatTable(schema, tableName)
+                : FormatTable(schema, tableName) + " " + encloseAlias;
+        }
+
+        public override string FormatTable(string schema, string tableName) {
+            if(schema != String.Empty)
+                return string.Format("{0}.{1}", EncloseAlias(schema, aliasLead, aliasEnd), EncloseAlias(tableName, aliasLead, aliasEnd));
+            if(string.IsNullOrEmpty(ObjectsOwner))
+                return aliasLead + tableName + aliasEnd;
+            return string.Format("{0}.{1}", EncloseAlias(ObjectsOwner, aliasLead, aliasEnd), EncloseAlias(tableName, aliasLead, aliasEnd));
+        }
+
+        string EncloseAlias(string alias, string lead, string end) {
+            if(String.IsNullOrEmpty(alias) || String.IsNullOrEmpty(lead) || String.IsNullOrEmpty(end))
+                return alias;
+            return String.Format("{0}{1}{2}", lead, EscapeAlias(alias, lead, end), end);
+        }
+
+        string EscapeAlias(string alias, string lead, string end) {
+            if(String.IsNullOrEmpty(alias) || String.IsNullOrEmpty(lead) || String.IsNullOrEmpty(end))
+                return alias;
+            string result = alias.Replace(lead, Twice(lead));
+            if(lead != end)
+                result = result.Replace(end, Twice(end));
+            return result;
+        }
+
+        string Twice(string value) {
+            return String.Format(CultureInfo.InvariantCulture, "{0}{0}", value);
+        }
+    }
+
+    public class CustomColumnDataLoader : ColumnarDataLoader {
+        public CustomColumnDataLoader(ISupportColumnarDataLoader provider) : base(provider) {}
+
+        public override void ProcessQuery(CancellationToken cancellationToken, Query query, Action<IDataReader, CancellationToken> action, IConnectionOptions connectionOptions) {
+            CreateAndExecuteQueryCommand(connectionOptions, query, command => {
+                using(IDataReader reader = command.ExecuteReader(CommandBehavior.Default)) {
+                    action(reader, cancellationToken);
+                }
+            });
+        }
+    }
+
+    public class CustomMsSqlProviderFactory : MSSqlProviderFactory {
+        public override IDataStore CreateProviderFromString(string connectionString, AutoCreateOption autoCreateOption, out IDisposable[] objectsToDisposeOnDisconnect) {
+            return CustomMsSqlConnectionProvider.CustomCreateProviderFromString(connectionString, autoCreateOption, out objectsToDisposeOnDisconnect);
+        }
+
+        public override IDataStore CreateProviderFromConnection(IDbConnection connection, AutoCreateOption autoCreateOption) {
+            return CustomMsSqlConnectionProvider.CreateCustomProviderFromConnection(connection, autoCreateOption);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to customize a Data Access provider
+
+
+<p>This example illustrates how to customize the <strong>DataAccessMsSqlConnectionProvider</strong> to use double quotation marks instead of square brackets for enclosing table names. This example also shows the use of <strong>CommandBehavior.Default</strong> by <strong>IDataReader</strong> during the query execution (by default, the Data Access library uses <strong>CommandBehavior.SequentialAccess</strong>). To run this example on your server, specify the required credentials in the invoked <strong>QBForm</strong>.</p>
+
+<br/>
+
+
